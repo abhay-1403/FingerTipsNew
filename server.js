9a7dd402b30e154a4af11cbaa9ba1a378ed31266
abhay-1403 <abhay.ban2002@gmail.com>
2022-12-03 18:13:28 +0530

    new1 commit

--- server.js
+++ server.js
@@ -5,25 +5,25 @@ const userRoute = require('./routes/userRoute')
 const app = express()
 app.use(express.json())
  const port = process.env.PORT || 5000
-//const port = 5000
+// const port = 5000
 
 //users and newsitems are a custom api which I have created and I am using router for modularization of
 //the code.
 app.use('/api/newsitems/' , newsRoute)
 app.use('/api/users/' , userRoute)
 
-// const path = require('path')
-// if(process.env.NODE_ENV === 'production'){
+const path = require('path')
+if(process.env.NODE_ENV === 'production'){
 
-//     app.use('/' , express.static('client/build'))
+    app.use('/' , express.static('client/build'))
 
-//     app.get('*' , (req , res)=>{
+    app.get('*' , (req , res)=>{
 
-//         res.sendFile(path.resolve(__dirname , 'client/build/index.html'))
+        res.sendFile(path.resolve(__dirname , 'client/index.html'))
 
-//     })
+    })
 
-// }
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
